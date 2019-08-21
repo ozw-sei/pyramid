53e3edb1ecde95da2106d4193fb7448119d73e3b
ozawaseijiro <seijiro4751@gmail.com>
2019-08-21 12:12:46 +0900

    add ini conf

---
+++ quick-tutorial/ini/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+requires = [
+    'pyramid',
+    'waitress'
+]
+
+setup(
+    name='tutorial',
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = tutorial:main'
+        ],
+    },
+
+)
